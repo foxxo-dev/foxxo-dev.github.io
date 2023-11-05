77f4c20636f7f1aeca446e5363edef8801b0cf3b
Adam Buksowicz <96523965+foxxo-dev@users.noreply.github.com>
2023-11-05 10:32:09 +0000

    First-Test

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-if (
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  )
-) {
-  window.location.href = 'mobile.html';
-}
+// if (
+//   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+//     navigator.userAgent
+//   )
+// ) {
+//   window.location.href = 'mobile.html';
+// }
