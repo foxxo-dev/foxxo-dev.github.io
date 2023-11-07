9f50651ec2631a0fbfe4e681aca832a6232e2c59
Foxxo Dev <gammert54@gmail.com>
2023-11-07 10:50:03 +0100

    Made Mobile Friendly Added 1 Tab

--- script.js
+++ script.js
@@ -5,3 +5,16 @@
 // ) {
 //   window.location.href = './pages/mobile.html';
 // }
+
+function copyText(text) {
+  const storage = document.createElement('textarea');
+  storage.value = text;
+  document.body.appendChild(storage);
+
+  // Copy the text in the fake `textarea` and remove the `textarea`
+  storage.select();
+  storage.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+  document.body.removeChild(storage);
+  alert('Copied to clipboard!');
+}
